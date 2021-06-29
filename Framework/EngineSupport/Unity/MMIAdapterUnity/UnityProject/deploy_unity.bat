edf6afb3751bfcbbe38cad7846931f2c45075b38
Janis Sprenger <Janis.Sprenger@dfki.de>
2021-06-29 10:07:54 +0200

    Updated deploy procedure for the MMIAdapterUnity to utilize Unity 2019.4.18f

--- Framework/EngineSupport/Unity/MMIAdapterUnity/UnityProject/deploy_unity.bat
+++ Framework/EngineSupport/Unity/MMIAdapterUnity/UnityProject/deploy_unity.bat
@@ -3,13 +3,19 @@ REM SPDX-License-Identifier: MIT
 REM The content of this file has been developed in the context of the MOSIM research project.
 REM Original author(s): Janis Sprenger, Bhuvaneshwaran Ilanthirayan
 
-if not defined UNITY2018_4_1 (
-  ECHO [31mUNITY2018_4_1 Environment variable pointing to the Unity.exe for Unity version 2018.4.1f1 is missing.[0m
-  ECHO    e.g. SET "UNITY2018_4_1=C:\Program Files\Unity Environments\2018.4.1f1\Editor\Unity.exe\"
+if not defined UNITY2019_18_1 (
+  ECHO [31mUNITY2019_18_1 Environment variable pointing to the Unity.exe for Unity version 2019.18.1f1 is missing.[0m
+  ECHO    e.g. SET "UNITY2019_18_1=C:\Program Files\Unity Environments\2018.4.1f1\Editor\Unity.exe\"
+  ECHO UNITY2019_18_1 defined as: "%UNITY2019_18_1%"
   pause
   exit /b 1
 ) else (
-  ECHO UNITY2018_4_1 defined as: "%UNITY2018_4_1%"
+  ECHO UNITY2019_18_1 defined as: "%UNITY2019_18_1%"
+  if not exist "%UNITY2019_18_1%" (
+    ECHO Unity does not seem to be installed or path name in deploy_variables.bat is wrong.
+    exit /b 2
+  )
+
 )
 
 IF EXIST build (
@@ -19,7 +25,7 @@ IF EXIST build (
 REM Build Unity Project:
 
 @REM call "%UNITY2018_4_1%" -batchmode -quit -logFile build.log -projectPath . -buildWindowsPlayer "build/UnityAdapter.exe"
-call "%UNITY2018_4_1%" -quit -batchmode -logFile build.log -projectPath "." -executeMethod BuildUnityAdapter.CreateServerBuild 
+call "%UNITY2019_18_1%" -quit -batchmode -logFile build.log -projectPath "." -executeMethod BuildUnityAdapter.CreateServerBuild 
 
 if %ERRORLEVEL% EQU 0 (
   COPY .\configurations\avatar.mos build\
