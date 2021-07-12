07ad51683c8ad2bb8056ccfce62764551ec39e04
Adam Kłodowski <adam.klodowski@lut.fi>
2021-07-12 12:09:13 +0300

    glTFExporter added to engine support

---
+++ Framework/EngineSupport/Unity/MMIUnity.TargetEngine/MMIUnity.TargetEngine.Editor/Editor_glTFExporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MMIUnity.TargetEngine.Scene;
+
+namespace MMIUnity.TargetEngine.Editor
+{
+    [CustomEditor(typeof(GLTFExport))]
+    public class Editor_glTFExport : UnityEditor.Editor
+    {
+        private MMISceneObject lastObject;
+
+        public override void OnInspectorGUI()
+        {
+            if ((lastObject != (target as GLTFExport).ObjectToExport) && (target as GLTFExport).ObjectToExport!=null)
+            {
+                (target as GLTFExport).GenerateFileName();
+                lastObject = (target as GLTFExport).ObjectToExport;
+            }
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Export object"))
+                (target as GLTFExport).ExportglTF();
+        }
+    }
+}
