84b8d602b58a4273e8682e61e36b505106a61ba5
Janis Sprenger <Janis.Sprenger@dfki.de>
2021-09-09 11:57:08 +0200

    Added testing MMU to test partial posture constraint

---
+++ BasicMMus/CS-MMUs/CS/PartialPostureTestMMU/PartialPostureTestMMU.cs
@@ -0,0 +1,64 @@
+﻿// SPDX-License-Identifier: MIT
+// The content of this file has been developed in the context of the MOSIM research project.
+// Original author(s): Felix Gaisbauer
+
+using MMICSharp.Common;
+using MMICSharp.Common.Attributes;
+using MMIStandard;
+using System;
+using System.Collections.Generic;
+
+namespace PartialPosture
+{
+    [MMUDescriptionAttribute("Janis Sprenger", "1.0", "PartialPostureTestMMU", "PartialPostureTest", "", "MMU for testing the partial posture constraints.", "MMU for testing the partial posture constraints.")]
+    public class PartialPostureTestMMU:MMUBase
+    {
+        private float blendDuration;
+        private float elapsed = 0;
+        private MInstruction instruction;
+
+        /// <summary>
+        /// Initialization method -> just call the base class
+        /// </summary>
+        /// <param name="avatarDescription"></param>
+        /// <param name="properties"></param>
+        /// <returns></returns>
+        public override MBoolResponse Initialize(MAvatarDescription avatarDescription, Dictionary<string, string> properties)
+        {
+            MBoolResponse response = base.Initialize(avatarDescription, properties);
+
+            //Initialize the skeleton access
+            this.SkeletonAccess = new IntermediateSkeleton();
+            this.SkeletonAccess.InitializeAnthropometry(avatarDescription);
+
+            return response;
+        }
+
+        public override MBoolResponse AssignInstruction(MInstruction instruction, MSimulationState simulationState)
+        {
+            base.AssignInstruction(instruction, simulationState);
+            this.instruction = instruction;
+            return new MBoolResponse(true);
+        }
+
+
+        public override MSimulationResult DoStep(double time, MSimulationState simulationState)
+        {
+
+            //Create a default result
+            MSimulationResult result = new MSimulationResult()
+            {
+                Events = new List<MSimulationEvent>(),
+                Constraints = simulationState.Constraints ?? new List<MConstraint>(),
+                SceneManipulations = new List<MSceneManipulation>(),
+                Posture = simulationState.Current
+            };
+
+            var data = new List<double>() { 0.707, 0.0, 0.0, 0.707 };
+            MAvatarPostureValues constraintPosture = new MAvatarPostureValues(result.Posture.AvatarID, data);
+            constraintPosture.PartialJointList = new List<MJointType>() { MJointType.LeftKnee };
+            result.Events.Add(new MSimulationEvent("EndTest", mmiConstants.MSimulationEvent_End, this.instruction.ID));
+            return result;
+        }
+    }
+}
