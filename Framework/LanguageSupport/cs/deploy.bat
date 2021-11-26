f9b3fdc7710b3a83fa39741276a10662f4d68050
Janis Sprenger <Janis.Sprenger@dfki.de>
2021-11-26 16:32:37 +0100

    removed redundant deploy scripts for the new deploy procedure

--- Framework/LanguageSupport/cs/deploy.bat
+++
@@ -1,8 +0,0 @@
-REM SPDX-License-Identifier: MIT
-REM The content of this file has been developed in the context of the MOSIM research project.
-REM Original author(s): Janis Sprenger, Bhuvaneshwaran Ilanthirayan
-
-REM Calling the visual studio deployement file
-
-call .\deploy_vs.bat
-exit /b %ERRORLEVEL%
